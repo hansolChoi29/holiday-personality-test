120d53d0526cf2a4b463d3e0a061a2a035794c56
jeongeun <ddo_eun@naver.com>
2024-11-29 19:26:39 +0900

    브랜치명 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <span>홈</span>
+      <span>로그아웃</span>
+      <span>결과보기</span>
+    </>
+  );
 };
 
 export default Header;
