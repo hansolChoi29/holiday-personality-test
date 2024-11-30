7925e4428fc805b59d2a9eb1954e832aa1d3ce8b
hansolChoi29 <ggksthf29@gmail.com>
2024-11-30 16:45:05 +0900

    feat:api

---
+++ src/api/results.js
@@ -0,0 +1,12 @@
+import { supabase } from "../supabase/supabase";
+
+export const saveTestResult = async ({ mbti, description }) => {
+  const userId = localStorage.getItem("userId");
+  if (!userId) throw new Error("User is not logged in.");
+
+  const { data, error } = await supabase
+    .from("results")
+    .insert([{ mbti, description, user_id: userId }]);
+  if (error) throw new Error(error.message);
+  return data;
+};
