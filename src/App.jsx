8fe009e8b336c17ab436bf67da6330f9200c1bd2
hansolChoi29 <ggksthf29@gmail.com>
2024-12-01 13:01:55 +0900

    feat:login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Homepage from "./pages/Homepage";
 import Joinpage from "./pages/Joinpage";
 import Loginpage from "./pages/Loginpage";
 import PrivateRoute from "./components/PrivateRoute";
@@ -13,9 +12,8 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/layout" element={<Layout />} />
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Loginpage />} />
         <Route path="/joinpage" element={<Joinpage />} />
-        <Route path="/loginpage" element={<Loginpage />} />
         <Route path="/testpage" element={<TestPage />} />
         <Route element={<PrivateRoute />}></Route>
         <Route path="/results" element={<Results />} />
